15faaacd7f991ac39a9629b9cc93e65b6d68d8b7
Saksham Hayaran <hsaksham0408@gmail.com>
2024-08-02 18:41:59 +0530

    JS added

---
+++ script.js
@@ -0,0 +1,30 @@
+//document.querySelector(".nav-logo").addEventListener("mouseover",function () {
+//    document.querySelector(".nav-logo").style.border = "2px solid white";
+//});
+//document.querySelector(".nav-address").addEventListener("mouseover",function () {
+//    document.querySelector(".nav-address").style.border = "2px solid white";
+//});
+//document.querySelector(".searchbar").addEventListener("mouseover",function () {
+//    document.querySelector(".searchbar").style.border = "2px solid #ffbb00";
+//});
+//document.querySelector(".searchbar").addEventListener("mouseover",function () {
+//    document.querySelector(".searchbar").style.border = "2px solid #ffbb00";
+//});
+//document.querySelector(".language").addEventListener("mouseover",function () {
+//    document.querySelector(".language").style.border = "2px solid white";
+//});
+//document.querySelector(".signin").addEventListener("mouseover",function () {
+//    document.querySelector(".signin").style.border = "2px solid white";
+//});
+//document.querySelector(".cart").addEventListener("mouseover",function () {
+//    document.querySelector(".cart").style.border = "2px solid white";
+//});
+//document.querySelector(".allmenu").addEventListener("mouseover",function () {
+//    document.querySelector(".allmenu").style.border = "2px solid white";
+//});
+//document.querySelector(".returnsandorders").addEventListener("mouseover",function () {
+//    document.querySelector(".returnsandorders").style.border = "2px solid white";
+//});
+//document.querySelector(".opsp").addEventListener("mouseover",function () {
+//    document.querySelector(".opsp").style.border = "2px solid white";
+//});
